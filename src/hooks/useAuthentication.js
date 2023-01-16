48abc7dca6acfc8d917ec7ad553676a79930a2d6
Wallison da Silva Gregorio <wallisongregorio2022@gmail.com>
2023-01-16 16:09:34 -0300

    pagina de login e logica de autenticação finalizada

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -86,11 +86,40 @@ export const useAuthentication = () => {
         signOut(auth);
     }
 
+    //login - sign in 
+
+    const login = async (data) => {
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError(false);
+
+        try {
+
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false);
+
+        } catch (error) {
+            let systemErrorMessage
+
+            if (error.message.includes('user-not-found')){
+                systemErrorMessage = 'Usuario não encontrado'
+            }else if (error.message.includes('wrong-password')){
+                systemErrorMessage = 'Senha incorreta'
+            }else{
+                systemErrorMessage = 'Ocorreu um erro, tente mais tarde!'
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+    }
+
     useEffect(()=>{
         return ()=> setCancelled(true);
     }, []);
 
-    return { auth, createUser, error, loading, logout};
+    return { auth, createUser, error, loading, logout, login};
 
 };
 
