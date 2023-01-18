07c6f1f8cec9f4863c0f8ea96ba8deae5f2e9a1b
Wallison da Silva Gregorio <wallisongregorio2022@gmail.com>
2023-01-18 10:57:57 -0300

    home page finalizada

---
+++ src/components/PostDetail.js
@@ -0,0 +1,24 @@
+import './PostDetail.css';
+
+import { Link } from 'react-router-dom';
+
+const PostDetail = ({post}) => {
+  return (
+    <div className='post_detail'>
+        <img src={post.image} alt={post.title} />
+        <h2>{post.title}</h2>
+        <p className='createdby'>{post.createdBy}</p>
+        <div className='tags'>
+            {post.arrayTags.map((tag) =>(
+                <p key={tag}>
+                    <span>#</span>
+                    {tag}
+                </p>
+            ))}
+        </div>
+        <Link to={`/post/${post.id}`} className="btn btn-outline">Ler</Link>
+    </div>
+  )
+}
+
+export default PostDetail;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,40 @@
 import './Home.css';
 
+//imporatando os hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
+//importando os componentes
+import PostDetail from '../../components/PostDetail';
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const {document: posts, loading} = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className='home'>
+        <h1>Veja nossos posts mais recentes.</h1>
+        <form onSubmit={handleSubmit} className='search_form'>
+          <input type="text" placeholder='Ou busque por tags...' onChange={(e) => e.target.value}/>
+          <button className='btn btn-dark'>Pesquisar</button>
+        </form>
+        <div>
+          {loading && <p>Carregando...</p>}
+          {posts && posts.map((post) => (
+            <PostDetail key={post.id} post={post}/>
+          ))}
+          {posts && posts.length === 0 && (
+            <div className="noposts">
+              <p>Não foram encontrado posts...</p>
+              <Link to="/post/create" className="btn">Criar primeiro post</Link>
+            </div>
+          )}
+        </div>
     </div>
   )
 }
